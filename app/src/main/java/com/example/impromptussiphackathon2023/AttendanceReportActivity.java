c269b43bc1e25bace9f8e6cf4ef6928a219fc27b
patelshivam <shivampatel3650@gmail.com>
2023-11-02 01:14:04 +0530

    Shivam attendance report

---
+++ app/src/main/java/com/example/impromptussiphackathon2023/AttendanceReportActivity.java
@@ -0,0 +1,14 @@
+package com.example.impromptussiphackathon2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AttendanceReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendance_report);
+    }
+}
