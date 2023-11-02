f7c7040b678fc869e1c26957a2ae7926479c95e5
patelshivam <shivampatel3650@gmail.com>
2023-11-02 22:32:52 +0530

    Shivam excel

--- app/src/main/java/com/example/impromptussiphackathon2023/AttendanceReportActivity.java
+++ app/src/main/java/com/example/impromptussiphackathon2023/AttendanceReportActivity.java
@@ -1,28 +1,120 @@
+//package com.example.impromptussiphackathon2023;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.os.Bundle;
+//import android.os.Environment;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.Toast;
+//
+//import org.apache.poi.ss.usermodel.*;
+//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+//
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//
+//import java.io.File;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//
+//import com.google.firebase.database.ValueEventListener;
+//
+//public class AttendanceReportActivity extends AppCompatActivity {
+//    private DatabaseReference databaseReference;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_attendance_report);
+//        Button myButton = findViewById(R.id.download);
+//        myButton.setOnClickListener(new View.OnClickListener(){
+//
+//            @Override
+//            public void onClick(View view) {
+//                databaseReference = FirebaseDatabase.getInstance().getReference().child("session_id_0");
+//
+//                ValueEventListener valueEventListener = databaseReference.addValueEventListener(new ValueEventListener() {
+//                    public void onDataChange(DataSnapshot dataSnapshot) {
+//                        if (dataSnapshot.exists()) {
+//                            String json = dataSnapshot.getValue().toString();
+//                            // Create an Excel file
+//                            try {
+//                                File file = new File(Environment.getExternalStorageDirectory(), "data.xlsx");
+//                                Workbook workbook = new XSSFWorkbook();
+//                                Sheet sheet = workbook.createSheet("data");
+//
+//                                // Assuming the JSON is in a format that can be directly converted to Excel
+//                                Row headerRow = sheet.createRow(0);
+//                                headerRow.createCell(0).setCellValue("division");
+//                                headerRow.createCell(1).setCellValue("group");
+//                                headerRow.createCell(2).setCellValue("period_date");
+//                                headerRow.createCell(3).setCellValue("period_end_time");
+//                                headerRow.createCell(4).setCellValue("period_start_time");
+//                                headerRow.createCell(5).setCellValue("subject_faculty");
+//                                headerRow.createCell(6).setCellValue("subject_name");
+//                                // Add more cells for other keys as needed
+//
+//                                // Parse and fill the Excel sheet
+//                                int rowNum = 1;
+//                                // You would parse your JSON and fill the Excel sheet here
+//                                // Example:
+//                                // for (JSONObject obj : yourParsedJsonArray) {
+//                                //    Row row = sheet.createRow(rowNum++);
+//                                //    row.createCell(0).setCellValue(obj.getString("key1"));
+//                                //    row.createCell(1).setCellValue(obj.getString("key2"));
+//                                //    // Add more cells for other keys as needed
+//                                // }
+//
+//                                // Write the Excel file
+//                                FileOutputStream fileOut = new FileOutputStream(file);
+//                                workbook.write(fileOut);
+//                                fileOut.close();
+//                                workbook.close();
+//                            } catch (IOException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+//                    }
+//
+//                    public void onCancelled(DatabaseError databaseError) {
+//                        // Handle errors if data retrieval fails
+//                    }
+//                });
+//            //    Toast.makeText(AttendanceReportActivity.this, "clicked", Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+//
+//    }
+//}
+
+
 package com.example.impromptussiphackathon2023;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Environment;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import android.widget.Button;
+public class AttendanceReportActivity extends AppCompatActivity{
 
-import com.google.firebase.database.ValueEventListener;
-
-public class AttendanceReportActivity extends AppCompatActivity {
     private DatabaseReference databaseReference;
 
     @Override
@@ -31,60 +123,72 @@ public class AttendanceReportActivity extends AppCompatActivity {
         setContentView(R.layout.activity_attendance_report);
         Button myButton = findViewById(R.id.download);
         myButton.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View view) {
-                databaseReference = FirebaseDatabase.getInstance().getReference().child("session_id_0");
+                // Initialize Firebase
+                FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                databaseReference = firebaseDatabase.getReference().child("session_id_0"); // Replace with your database node
 
-                ValueEventListener valueEventListener = databaseReference.addValueEventListener(new ValueEventListener() {
+                // Retrieve data from Firebase and create Excel
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         if (dataSnapshot.exists()) {
-                            String json = dataSnapshot.getValue().toString();
-
-                            // Create an Excel file
                             try {
-                                File file = new File(Environment.getExternalStorageDirectory(), "firebase_data.xlsx");
+                                File file = new File(Environment.getExternalStorageDirectory(), "data.xlsx");
                                 Workbook workbook = new XSSFWorkbook();
-                                Sheet sheet = workbook.createSheet("Firebase Data");
+                                Sheet sheet = workbook.createSheet("Data");
 
-                                // Assuming the JSON is in a format that can be directly converted to Excel
-                                Row headerRow = sheet.createRow(0);
-                                headerRow.createCell(0).setCellValue("division");
-                                headerRow.createCell(1).setCellValue("group");
-                                headerRow.createCell(2).setCellValue("period_date");
-                                headerRow.createCell(3).setCellValue("period_end_time");
-                                headerRow.createCell(4).setCellValue("period_start_time");
-                                headerRow.createCell(5).setCellValue("subject_faculty");
-                                headerRow.createCell(6).setCellValue("subject_name");
-                                // Add more cells for other keys as needed
+                                int rowNum = 0;
 
-                                // Parse and fill the Excel sheet
-                                int rowNum = 1;
-                                // You would parse your JSON and fill the Excel sheet here
-                                // Example:
-                                // for (JSONObject obj : yourParsedJsonArray) {
-                                //    Row row = sheet.createRow(rowNum++);
-                                //    row.createCell(0).setCellValue(obj.getString("key1"));
-                                //    row.createCell(1).setCellValue(obj.getString("key2"));
-                                //    // Add more cells for other keys as needed
-                                // }
+                                for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
+                                    Row row = sheet.createRow(rowNum++);
+                                    // Example: Fetching 'name' and 'age' fields, adjust as per your data structure
+                                    String div = childSnapshot.child("division").getValue(String.class);
+                                    String grp = childSnapshot.child("group").getValue(String.class);
+                                    String period_date = childSnapshot.child("period_date").getValue(String.class);
+                                    String period_end_time = childSnapshot.child("period_end_time").getValue(String.class);
+                                    String period_start_time = childSnapshot.child("period_start_time").getValue(String.class);
+                                    String subject_faculty = childSnapshot.child("subject_faculty").getValue(String.class);
+                                    String subject_name = childSnapshot.child("subject_name").getValue(String.class);
+
+                                    // Adding data to Excel rows
+                                    row.createCell(0).setCellValue(div);
+                                    row.createCell(1).setCellValue(grp);
+                                    row.createCell(2).setCellValue(period_date);
+                                    row.createCell(3).setCellValue(period_end_time);
+                                    row.createCell(4).setCellValue(period_start_time);
+                                    row.createCell(5).setCellValue(subject_faculty);
+                                    row.createCell(6).setCellValue(subject_name);
+
+
+                                    // Add more cells for other data fields
+
+                                    // Adjust column index and cell values according to your data
+                                }
 
                                 // Write the Excel file
                                 FileOutputStream fileOut = new FileOutputStream(file);
                                 workbook.write(fileOut);
                                 fileOut.close();
                                 workbook.close();
+
+                                Toast.makeText(getApplicationContext(), "Excel file created", Toast.LENGTH_SHORT).show();
+
                             } catch (IOException e) {
                                 e.printStackTrace();
+                                Toast.makeText(getApplicationContext(), "Error creating Excel file", Toast.LENGTH_SHORT).show();
                             }
                         }
                     }
 
+                    @Override
                     public void onCancelled(DatabaseError databaseError) {
-                        // Handle errors if data retrieval fails
+                        Toast.makeText(getApplicationContext(), "Firebase Data retrieval error", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
+
         });
 
     }
