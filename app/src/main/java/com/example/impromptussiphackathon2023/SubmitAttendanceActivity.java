16946861044aca0b7f0a905b15d319f90959184c
Aryan <64796509+aryanranderiya@users.noreply.github.com>
2023-11-04 00:32:24 +0530

    submit attendance wifi implementation

--- app/src/main/java/com/example/impromptussiphackathon2023/SubmitAttendanceActivity.java
+++ app/src/main/java/com/example/impromptussiphackathon2023/SubmitAttendanceActivity.java
@@ -2,15 +2,20 @@ package com.example.impromptussiphackathon2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SubmitAttendanceActivity extends AppCompatActivity {
-Button btn_logout;
+    Button btn_logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,21 +26,38 @@ Button btn_logout;
     }
 
     private void checkWifi() {
-        startActivity(new Intent(getApplicationContext(),));
+        if (isConnectedToSpecificNetwork("9c:53:22:3a:46:d9")) {
+            startActivity(new Intent(getApplicationContext(), activity_select_attendance.class));
+        } else {
+            Toast.makeText(this, "Not connected to the University Network.", Toast.LENGTH_LONG).show();
+        }
     }
 
-    private void Init() {
-        btn_logout = findViewById(R.id.btn_logout);
-        btn_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                logOut();
+    private boolean isConnectedToSpecificNetwork(String specificBSSID) {
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        if (wifiManager != null) {
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            String currentBSSID = wifiInfo.getBSSID();
+            Toast.makeText(this, currentBSSID, Toast.LENGTH_SHORT).show();
+
+            if (specificBSSID.equals(currentBSSID)) {
+                return true;
             }
-        });
+        }
+        return false;
     }
+        private void Init () {
+            btn_logout = findViewById(R.id.btn_logout);
+            btn_logout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    logOut();
+                }
+            });
+        }
 
-    public void logOut() {
-        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-        FirebaseAuth.getInstance().signOut();
+        public void logOut() {
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            FirebaseAuth.getInstance().signOut();
+        }
     }
-}
