5f5047a545cd4f6a0a4ba9fe4e5a753b748af0cf
Aryan <64796509+aryanranderiya@users.noreply.github.com>
2023-11-04 02:11:57 +0530

    final commit

--- app/src/main/java/com/example/impromptussiphackathon2023/TakeAttendanceActivity.java
+++ app/src/main/java/com/example/impromptussiphackathon2023/TakeAttendanceActivity.java
@@ -17,7 +17,7 @@ public class TakeAttendanceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_take_attendace);
+        setContentView(R.layout.activity_take_attendance);
 
         Init();
         Buttons();
