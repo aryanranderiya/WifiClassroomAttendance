36373b8212ef3e85107ed0832797c83228044e41
Aryan <64796509+aryanranderiya@users.noreply.github.com>
2023-11-04 13:06:29 +0530

    BSSID validation is working

--- app/src/main/java/com/example/impromptussiphackathon2023/SubmitAttendanceActivity.java
+++ app/src/main/java/com/example/impromptussiphackathon2023/SubmitAttendanceActivity.java
@@ -2,10 +2,14 @@ package com.example.impromptussiphackathon2023;
 
 import static java.lang.Thread.sleep;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
@@ -29,12 +33,7 @@ public class SubmitAttendanceActivity extends AppCompatActivity {
     }
 
     private void checkWifi() {
-        try {
-            sleep(3);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        if (isConnectedToSpecificNetwork("9c:53:22:3a:46:d9")) {
+        if (isConnectedToSpecificNetwork("90:4c:81:d9:48:93")) {
             startActivity(new Intent(getApplicationContext(), SelectAttendanceActivity.class));
         } else {
             Toast.makeText(this, "Not connected to the University Network.", Toast.LENGTH_LONG).show();
@@ -45,8 +44,7 @@ public class SubmitAttendanceActivity extends AppCompatActivity {
         WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         if (wifiManager != null) {
             WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-//            String currentBSSID = wifiInfo.getBSSID();
-            String currentBSSID = specificBSSID; // temporary assign the current to the specific BSSID
+            String currentBSSID = wifiInfo.getBSSID();
 
             Log.d("aryanranderiya",currentBSSID);
 
@@ -69,5 +67,38 @@ public class SubmitAttendanceActivity extends AppCompatActivity {
         public void logOut() {
             startActivity(new Intent(getApplicationContext(), LoginActivity.class));
             FirebaseAuth.getInstance().signOut();
+        }
+
+        public void locationAccess(){
+            if (hasLocationPermissions()) {
+                checkWifi();
+            } else {
+                requestLocationPermissions();
+            }
+        }
+
+    private boolean hasLocationPermissions() {
+        return ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 123; // You can use any value
+
+    private void requestLocationPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                LOCATION_PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                checkWifi();
+            } else {
+                Toast.makeText(this, "Please enable Location permissions", Toast.LENGTH_SHORT).show();
+            }
+        }
         }
     }
